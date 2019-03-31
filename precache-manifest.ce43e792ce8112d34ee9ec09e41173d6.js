4fe5f17f6d1d9eea507546909f1e1766c12fdc02
SupViz <44785095+SupViz@users.noreply.github.com>
2019-03-31 13:00:00 +0200

    update 1.2.1

---
+++ precache-manifest.ce43e792ce8112d34ee9ec09e41173d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a02b40ebcd74474074a6",
+    "url": "https://supviz.infos.st/static/js/runtime~main.11b32fb6.js"
+  },
+  {
+    "revision": "f7993229010e984c429b",
+    "url": "https://supviz.infos.st/static/js/main.a7ad0d9a.chunk.js"
+  },
+  {
+    "revision": "d4d2e80d437d47b6760d",
+    "url": "https://supviz.infos.st/static/js/2.01bda724.chunk.js"
+  },
+  {
+    "revision": "f7993229010e984c429b",
+    "url": "https://supviz.infos.st/static/css/main.7558ef35.chunk.css"
+  },
+  {
+    "revision": "f52411a74bbdcdb6c8a97bc78d7819c9",
+    "url": "https://supviz.infos.st/index.html"
+  }
+];
